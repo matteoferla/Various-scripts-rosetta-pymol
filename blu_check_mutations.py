e244596d6e23abf030ea66924139b29c24302eff
Matteo Ferla <matteo.ferla@gmail.com>
2017-08-18 16:48:48 +0100

    Update blu_check_mutations.py

--- blu_check_mutations.py
+++ blu_check_mutations.py
@@ -15,13 +15,21 @@ from pprint import PrettyPrinter
 
 pp = PrettyPrinter()
 
-
+import argparse
 #import csv, os, re
 #from Bio import SeqIO
 
+def blu(infile):
+    for line in [line.split() for line in open(infile)]:
+        if line[2] != '.':
+            if line[3] != 'PIKAA' or line[3] != 'NATAA':
+                print(line[1]+line[0]+' '+line[3])
+            if line[3] == 'PIKAA' and line[1] != line[4]:
+                print(line[1]+line[0]+line[4])
 
-#1 V D PIKAA V
-for line in [line.split() for line in open('toSpyC2.1.blu')]:
-    if line[2] != '.':
-        if line[1] != line[4]:
-            print(line[1]+line[0]+line[4])
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=__description__)
+    parser.add_argument("input", help="the input blue file")
+    parser.add_argument('--version', action='version', version=__version__)
+    args = parser.parse_args()
+    blu(args.input)
